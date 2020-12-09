3d349fc6d01d8328717a8c55b93e780592d18f10
TheodoreLerondeau <50512126+TheodoreLerondeau@users.noreply.github.com>
2020-12-09 17:46:54 +0100

    Update headPose.py

--- headPose.py
+++ headPose.py
@@ -1,14 +1,20 @@
 import cv2
-from headPose_Detection import headpose_detect_func
+from headPose_Detection import headpose_detect_func, detect_position
 
 cap = cv2.VideoCapture(0)
-    
+
+ret, frame = cap.read() 
+
 while(True):
     ret, frame = cap.read()
-    headpose_detect_func(frame)
+       
+    diff_x, diff_y = headpose_detect_func(frame)
+    
+    if diff_x is not None and diff_y is not None:
+        detect_position(diff_x, diff_y)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
