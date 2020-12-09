c49e52649c9b6322e5f754389773327bcb443792
TheodoreLerondeau <50512126+TheodoreLerondeau@users.noreply.github.com>
2020-12-09 15:10:36 +0100

    Update headPose.py

--- headPose.py
+++ headPose.py
@@ -1,25 +1,11 @@
-import numpy as np
 import cv2
+from headPose_Detection import headpose_detect_func
 
 cap = cv2.VideoCapture(0)
-
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-
+    
 while(True):
-    # Capture frame-by-frame
     ret, frame = cap.read()
-    
-    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
-    
-    for (x,y,w,h) in faces:
-        gray = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        gray = cv2.circle(frame, (int(x+w/2),int(y+h/2)), 5,(0, 0, 255), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = gray[y:y+h, x:x+w]
-     
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
+    headpose_detect_func(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
