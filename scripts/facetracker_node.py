fa34f960cb6eefef6c8722dfd14bef126f94f0a4
MammothOG <felix.capuano@gmail.com>
2020-12-26 22:08:12 +0100

    adding target

--- scripts/facetracker_node.py
+++ scripts/facetracker_node.py
@@ -4,31 +4,62 @@ import cv2
 import numpy as np
 from simple_head_pos import get_heads_pos
 
+TARGET_SIZE = 20
+
+def in_target(img_centre, pt):
+    if img_centre[0]+TARGET_SIZE > pt[0] and \
+        img_centre[0]-TARGET_SIZE < pt[0] and \
+        img_centre[1]+TARGET_SIZE > pt[1] and \
+        img_centre[1]-TARGET_SIZE < pt[1]:
+        return True
+    else:
+        return False
+
 def process_img(cv_image):
-    heads_pos = get_heads_pos(cv_image)
-    # peut etre faire la moyenne si deux tete sont detecté
     vect_norm = np.zeros(2)
 
+    # on cherche la position des tetes sur l'image
+    heads_pos = get_heads_pos(cv_image)
+
+    # on calcule le centre de l'image
+    height, width, _ = cv_image.shape
+    img_center = (width//2, height//2)
+
+    # affichage de la cible
+    cv2.rectangle(cv_image,
+                  (img_center[0]-TARGET_SIZE, img_center[1]-TARGET_SIZE),
+                  (img_center[0]+TARGET_SIZE, img_center[1]+TARGET_SIZE),
+                  (0, 0, 255), 1)
+
+    # si aucune tete n'est detectée
     if len(heads_pos) > 0:
-        w_head_pos, h_head_pos = heads_pos[0]
+        # je prend seulement la premier tete detecter ici
+        # peut etre faire la moyenne si deux tete sont detecté
+        head_pos = heads_pos[0]
 
-        height, width, channels = cv_image.shape
-        w_img_center, h_img_center = width//2, height//2
+        # si la tete est suffisament centrée
+        if not in_target(img_center, head_pos):
+            print("not in target")
 
-        # vecteur direction du centre par rapport a la position de la tête
-        vect = np.array([w_img_center-w_head_pos, h_head_pos-h_img_center])
+            # vecteur direction du centre par rapport a la position de la tête
+            vect = np.array([img_center[0]-head_pos[0], head_pos[1]-img_center[1]])
+
+            # normalisation du vecteur
+            vect_norm = vect / np.sqrt(np.sum(vect**2))
+
+            # publish HERE
+        else:
+            print("in target")
+
+        # affichage du vecteur
         cv2.arrowedLine(cv_image,
-                        (w_img_center, h_img_center),
-                        (w_head_pos, h_head_pos),
+                        (img_center[0], img_center[1]),
+                        (head_pos[0], head_pos[1]),
                         (255, 0, 0), 2)
 
-        # normalisation du vecteur
-        vect_norm = vect / np.sqrt(np.sum(vect**2))
-
-    # Display the resulting frame
+    # affiche l'image
     cv2.imshow('camera', cv_image)
     cv2.waitKey(1)
-
 
 if __name__ == '__main__':
     import rospy
